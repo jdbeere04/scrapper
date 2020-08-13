b7bbb83c6f828cebf30f7dea1c56e78d345031be
jdbeere04 <60322993+jdbeere04@users.noreply.github.com>
2020-08-13 21:20:48 +0100

    Create scrapper.php

---
+++ scrapper.php
@@ -0,0 +1,36 @@
+<?php
+        include("simple_html_dom.php");
+?>
+<!DOCTYPE html>
+    <head>
+        <div class = "inputs" type = "container">
+            <form method = "get">
+                <input type = "text" name = "ID"><br>
+                <input type = "submit" name = "submit"><br>
+            </form>
+        </div>
+    </head>
+    <body>
+        <?php
+
+            if(isset($_GET["submit"]))
+            {
+                $id = $_GET["ID"];
+                $html = file_get_html("https://www.shutterstock.com/image-illustration/" . $id);
+                    if(!$id){
+                        
+                        die();
+
+                    } elseif  ($id > 0){
+
+                        $list = $html->find('div[class="C_a_03061"]', 0);
+                        foreach( $list->find('a') as $element ){
+                            echo $element -> plaintext;
+                            echo "<br>";
+                        }
+
+                    }
+            } 
+        ?>
+    </body>
+</html>
